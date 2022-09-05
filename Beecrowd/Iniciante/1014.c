c49bc6e99eb562cf6c3ed28f64d9f2959029f60f
Elrate <contatogabrielvc@gmail.com>
2022-09-05 01:15:21 -0300

    Implementation of consumption

---
+++ Beecrowd/Iniciante/1014.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int x;
+    float y, media;
+    
+    scanf("%d", &x);
+    scanf("%f", &y);
+    
+    media = x/y;
+    
+    printf("%.3f km/l\n", media);
+    
+    return 0;
+}
