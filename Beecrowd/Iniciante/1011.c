31cc0a7e72ebd014db1ed75c0fd043fb39d419a1
Elrate <contatogabrielvc@gmail.com>
2022-09-05 01:09:20 -0300

    Area implementation

---
+++ Beecrowd/Iniciante/1011.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double a, b;
+    
+    scanf("%lf", &a);
+    
+    b = (4 * 3.14159 * (a * a * a))/3.0;
+   
+    printf("VOLUME = %.3lf\n", b);
+ 
+    return 0;
+}
