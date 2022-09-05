27547235ec5b5ce4d467d9b9cea4bb3b6db21dcd
Elrate <contatogabrielvc@gmail.com>
2022-09-05 01:03:39 -0300

    Circle area implementation

---
+++ Beecrowd/Iniciante/1002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double n, area;
+    
+    scanf("%lf", &n);
+    
+    area = 3.14159 * (n * n);
+    
+    printf("A=%.4f\n",area);
+ 
+ 
+    return 0;
+}
