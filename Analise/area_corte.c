de9a749174df302a3185036d30fdfd3f22b2196c
Elrate <contatogabrielvc@gmail.com>
2023-03-17 23:26:14 -0300

    Simplificação do código

--- Analise/area_corte.c
+++ Analise/area_corte.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void busca_binaria(double vetor[],double area_esp,double inicio,int qtd_placas,double maior,double *metade);
-double soma_cima(double meio,double vetor[],int qtd_placas);
+void busca_binaria(double vetor[],double area_esp,int qtd_placas,double lim_inferior,double lim_superior,double *metade);
+double soma_cima(double vetor[],double metade,int qtd_placas);
 
 int main(){
 
@@ -30,7 +30,7 @@ int main(){
     else if(area_total < area_esp)
         printf("-.-\n");
     else{
-        busca_binaria(vetor,area_esp,0,qtd_placas,maior,&metade);
+        busca_binaria(vetor,area_esp,qtd_placas,0,maior,&metade);
         printf("%.4lf\n",metade);
         printf(":D\n");
     }
@@ -38,28 +38,28 @@ int main(){
     return 0;
 }
 
-void busca_binaria(double vetor[],double area_esp,double inicio, int qtd_placas,double maior,double *metade){
+void busca_binaria(double vetor[],double area_esp,int qtd_placas,double lim_inferior,double lim_superior,double *metade){
 
     double area_calculada;
 
-    while((maior - inicio) >= 0.00000000001){
+    while((lim_superior - lim_inferior) >= 0.00000000001){
         
-        (*metade) = (inicio + maior)/2;
+        (*metade) = (lim_inferior + lim_superior)/2;
 
-        area_calculada = soma_cima((*metade),vetor,qtd_placas);
+        area_calculada = soma_cima(vetor,(*metade),qtd_placas);
         if(area_calculada == area_esp)
             return;
         else{
             if(area_calculada > area_esp)
-                inicio = (*metade);
+                lim_inferior = (*metade);
             else
-                maior = (*metade);
+                lim_superior = (*metade);
         }
     }
     return;
 }
 
-double soma_cima(double metade,double vetor[],int qtd_placas){
+double soma_cima(double vetor[],double metade,int qtd_placas){
 
     double soma_areas= 0;
 
@@ -68,7 +68,7 @@ double soma_cima(double metade,double vetor[],int qtd_placas){
     for(int i=0;i<qtd_placas;i++){
         area_sobrou = vetor[i] - metade;
         if (area_sobrou < 0)
-            area_sobrou =0;
+            area_sobrou = 0;
 
         soma_areas = soma_areas + area_sobrou;
     }
